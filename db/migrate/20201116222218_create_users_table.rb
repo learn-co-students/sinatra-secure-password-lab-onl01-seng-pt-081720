0985f4bdd5d43a8f27b3ae0fa88fdaf716e39004
Brenda Ferrufino <ferrufinob@gmail.com>
2020-11-16 18:08:13 -0500

    Done.

---
+++ db/migrate/20201116222218_create_users_table.rb
@@ -0,0 +1,8 @@
+class CreateUsersTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |user|
+      user.string :username
+      user.string :password_digest
+    end
+  end
+end
