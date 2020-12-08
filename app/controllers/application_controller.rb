929f87e9d6c544fd795950848656c381b147463a
Janaeq <janaemassa@gmail.com>
2020-12-08 01:07:05 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,37 +9,55 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+    # renders an `index.erb` file with links to signup or login. 
     erb :index
   end
 
   get "/signup" do
+    # renders a form to create a new user. The form includes fields for `username` and `password`
     erb :signup
   end
 
   post "/signup" do
     #your code here
-
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+      redirect to '/account'
+    else
+      redirect to '/failure'
+    end 
   end
 
   get '/account' do
+    # renders an `account.erb` page, which should be displayed once a user successfully logs in.
     @user = User.find(session[:user_id])
     erb :account
   end
 
 
   get "/login" do
+    # renders a form for logging in.
     erb :login
   end
 
   post "/login" do
-    ##your code here
+    #your code here
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/account'
+    else
+      redirect to '/failure'
+    end
   end
 
   get "/failure" do
+    # renders a `failure.erb` page. This will be accessed if there is an error logging in or signing up. 
     erb :failure
   end
 
   get "/logout" do
+    # clears the session data and redirects to the home page.
     session.clear
     redirect "/"
   end
